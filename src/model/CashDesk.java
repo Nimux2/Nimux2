e9d6eaff5ba3dc58f9f90819fe0fd6c6d269705f
GOEPP THOMAS <thomas.g@webcd.fr>
2024-02-13 17:04:34 +0100

    CashDesk

--- src/model/CashDesk.java
+++ src/model/CashDesk.java
@@ -10,6 +10,7 @@ public class CashDesk {
 	private Table _activeTable;
 	private List<Table> _cashedTables;
 	private List<CashDeskObserver> _observers;
+	private List<Table> _allTables;
 
 	public CashDesk() {
 		_priceOfSoldBeers = 0.;
@@ -17,6 +18,7 @@ public class CashDesk {
 		_activeTable = new Table();
 		_cashedTables = new ArrayList<>();
 		_observers = new ArrayList<>();
+		_allTables = new ArrayList<>();
 	}
 
 	public void cashTable() {
@@ -28,7 +30,6 @@ public class CashDesk {
 		// Ajouter la table active à la liste des tables encaissées
 		_cashedTables.add(_activeTable);
 		// Créer une nouvelle table active
-		_activeTable = new Table();
 		// Notifier les opbserveurs
 		notifyCashTable(_cashedTables.size(), _priceOfSoldBeers, _priceOfSoldTartesFlambees);
 	}
@@ -76,6 +77,32 @@ public class CashDesk {
 	private void notifyReset() {
 		for (CashDeskObserver observer : _observers) {
 			observer.updateReset();
+		}
+	}
+
+	public void setCurrentTable(Table table) {
+		this._activeTable = table;
+		notifyChangeTable();
+	}
+
+	public List<Table> getAllTables() {
+		return this._allTables;
+	}
+
+	public void addTable() {
+		this._allTables.add(new Table());
+		notifyModifyListTable();
+	}
+
+	private void notifyModifyListTable() {
+		for (CashDeskObserver observer : _observers) {
+			observer.updateListTable(this._allTables);
+		}
+	}
+
+	private void notifyChangeTable(){
+		for (CashDeskObserver observer : _observers) {
+			observer.updateCurrentTable(this._activeTable);
 		}
 	}
 }
